f2ed70801e7d9f21de46fe0d2e7a38a72bb8cac2
kjj-kikuchi <a20.jtkf@g.chuo-u.ac.jp>
2024-11-07 15:04:15 +0900

    Initial Commit

---
+++ simpleQEM-simplification/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simpleQEM-simplification
+//
+//  Created by 菊池祐作 on 2024/11/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
