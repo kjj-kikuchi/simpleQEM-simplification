f2babbc0fc94bb3a6f876147d4452a0878471d1c
Yusaku Kikuchi <a20.jtkf@g.chuo-u.ac.jp>
2025-02-19 15:24:20 +0900

    Update readmesh.hpp

--- source/readmesh.hpp
+++ source/readmesh.hpp
@@ -8,6 +8,14 @@
 #ifndef readmesh_hpp
 #define readmesh_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include "mesh.hpp"
+
+namespace ReadMesh
+{
+void read_obj(std::string const& filename, Mesh& mesh);
+void export_obj(std::string name, std::vector<Eigen::Vector3d> const& vert, std::vector<Eigen::Vector3i> const& face);
+}
 
 #endif /* readmesh_hpp */
