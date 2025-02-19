ca379120840bb974d3bb8190c986ef6cc3e15f59
Yusaku Kikuchi <a20.jtkf@g.chuo-u.ac.jp>
2025-02-19 15:22:30 +0900

    Update mesh.cpp

--- source/mesh.cpp
+++ source/mesh.cpp
@@ -6,3 +6,21 @@
 //
 
 #include "mesh.hpp"
+
+void Mesh::make_edge_map()
+{
+    int edge_idx = 0;
+    for (auto& f : F)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            auto edge = std::make_pair(f(j), f((j+1)%3));
+            auto edge_opp = std::make_pair(f((j+1)%3), f(j));
+            if (! E.contains(edge) && ! E.contains(edge_opp))
+            {
+                E.emplace(edge, edge_idx);
+                edge_idx ++;
+            }
+        }
+    }
+}
