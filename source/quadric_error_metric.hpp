626f63642106a339b13a833c66372610c12b3b8f
Yusaku Kikuchi <a20.jtkf@g.chuo-u.ac.jp>
2025-02-19 15:23:41 +0900

    Update quadric_error_metric.hpp

--- source/quadric_error_metric.hpp
+++ source/quadric_error_metric.hpp
@@ -8,6 +8,27 @@
 #ifndef quadric_error_metric_hpp
 #define quadric_error_metric_hpp
 
-#include <stdio.h>
+#include <Eigen/Geometry>
+#include <Eigen/LU>
+#include "mesh.hpp"
+
+struct QEM
+{
+    Eigen::Matrix3d A;
+    Eigen::Vector3d b;
+    double c;
+
+    Eigen::Vector3d minimizer();
+
+    double min_value();
+};
+
+QEM compute_face_qem(Mesh const& m, Eigen::Vector3i f);
+
+QEM operator+(QEM q1, QEM q2);
+
+QEM operator+=(QEM q1, QEM q2);
+
+QEM operator/(QEM q1, double val);
 
 #endif /* quadric_error_metric_hpp */
