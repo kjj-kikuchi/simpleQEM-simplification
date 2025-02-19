b9112964b66b8d42f3307596773b62e1e4999903
Yusaku Kikuchi <a20.jtkf@g.chuo-u.ac.jp>
2025-02-19 15:24:49 +0900

    Update simplification.hpp

--- source/simplification.hpp
+++ source/simplification.hpp
@@ -8,6 +8,19 @@
 #ifndef simplification_hpp
 #define simplification_hpp
 
-#include <stdio.h>
+#include <queue>
+#include "mesh.hpp"
+#include "quadric_error_metric.hpp"
+#include "union_find.hpp"
+
+class Simplification
+{
+    std::vector<QEM> Qv;
+public:
+    std::vector<Eigen::Vector3d> coarseV;
+    std::vector<Eigen::Vector3i> coarseF;
+
+    void compute(Mesh const& m, int const target_v_num);
+};
 
 #endif /* simplification_hpp */
